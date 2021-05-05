cd7c5bf87ee610ef701c3a89e59fec586932122b
SoilFoil64 <75800335+SoilFoil64@users.noreply.github.com>
2021-05-05 10:09:52 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
-  name = 'extvars',         # How you named your package folder (MyLib)
-  packages = ['extvars'],   # Chose the same as "name"
+  name = 'ext-var',         # How you named your package folder (MyLib)
+  packages = ['ext-var'],   # Chose the same as "name"
   version = '1.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'External Variables in Python.',   # Give a short description about your library
   author = 'Cube.py',                   # Type in your name
   author_email = 'kisto1981.2010@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/SoilFoil64/extvar',   # Provide either the link to your github or to your website
-  download_url = 'https://Snips.soilfoil.repl.co/extvars-v1.0.tar.gz',    # I explain this later on
+  url = 'https://github.com/SoilFoil64/ext-var',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/SoilFoil64/ext-var/archive/refs/tags/v1.0.tar.gz',    # I explain this later on
   keywords = ['variables', 'files'],   # Keywords that define your package best
   install_requires=[  ],
   classifiers=[
